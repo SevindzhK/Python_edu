fba4b83340f7c5c43530783b18eb5a8b9aa91aa5
SevindzhK <sevinge0709@gmail.com>
2022-12-22 14:49:25 +0300

    Change folder's  name

--- Algorithms.Part1/hw1_1.py
+++ Algorithms.Part1/hw1_1.py
@@ -3,7 +3,7 @@
 #Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 
 
 number = int(input('Enter day of week (number from 1 to 7): '))
-if (number < 1 or number > 8):
-	print('You entered incorrect number')
+if (number == 6 or number == 7):
+	print('Today is a day off')
 else:
-	print(number == 6 or number == 7)
+	print('You entered incorrect number')
--- Algorithms.Part1/hw1_5.py
+++ Algorithms.Part1/hw1_5.py
@@ -9,5 +9,5 @@ x2 = int(input('Enter x2:'))
 y2 = int(input('Enter y2:'))
 
 distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-#print(round(distance, 2))   
-print(format(distance, '.2f'))   # format выдает абсолютно такие же значения, как round (округляет 5,09 до 5.10). Как все-таки вывести 5.09? 
+
+print(format(distance, '.2f'))   
